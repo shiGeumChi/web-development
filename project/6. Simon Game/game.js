697ca416cdf7b8f158d223c52323d68ae8d86770
shiGeumChi <75241209+shiGeumChi@users.noreply.github.com>
2021-09-24 18:39:13 +0900

    Add files via upload

---
+++ project/6. Simon Game/game.js
@@ -0,0 +1,82 @@
+var gamePattern = [];
+var userClickedPattern = [];
+
+buttonColours = ["red", "blue", "green", "yellow"];
+
+var level = 0;
+var flag = false;
+
+$(document).on("keypress", function() {
+  if (!flag) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    flag = true;
+  }
+})
+
+
+$(".btn").click(function(e) {
+  var userChosenColour = e.target.id;
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function nextSequence() {
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $('#' + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $('#' + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $('#' + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    playSound("wrong");
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  flag = false;
+}
